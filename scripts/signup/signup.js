0d12cd9069f73a30acaf71b940263eb1edee0504
GiMGalvao <giovannamgalvao@gmail.com>
2022-04-19 22:39:53 -0300

    Atualização CheckPOint

--- scripts/signup/signup.js
+++ scripts/signup/signup.js
@@ -1,68 +1,78 @@
-let nomeCriarConta = document.getElementById("nome");
-let apelidoCriarConta = document.getElementById("apelido");
-let emailCriarConta = document.getElementById("email");
-let senhaCriarConta = document.getElementById("senha");
-//let repetirSenhaCriarConta = getElementById("repetirSenha");
-
-let botaoCadastro = document.getElementById("botaoCadastro")
-
-const userObjCadastro = {
-firstName: "",
-lastName: "",
-email: "",
-password: ""
-};
-
-botaoCadastro.addEventListener('click', function(event) {
-
-event.preventDefault();
-
-if(nomeCriarConta.value !="" && apelidoCriarConta.value != "" && 
-emailCriarConta.value != "" && senhaCriarConta.value != "") {
-userObjCadastro.firstName = nomeCriarConta.value;
-userObjCadastro.lastName = apelidoCriarConta.value;
-userObjCadastro.email = emailCriarConta.value;
-userObjCadastro.password = senhaCriarConta.value;
-
-let objetoUserJson = JSON.stringify(userObjCadastro);
-
-let configurRequisicao = {
-method: 'POST',
-body: objetoUserJson ,
-headers: {
-'content-type':'application/json'
-},
-};
-
-
-fetch("https://ctd-todo-api.herokuapp.com/v1/users", configurRequisicao)
-. then((response) => {
-if(response.status == 201) {
-return response.json()
-}
-throw response;
-
-}).then(function (resposta) {
-cadastroSucesso(resposta.jwt)
-})
-.catch(error => {
-cadastroErro(error)
-});
-} 
-else {
-alert("Preencha todos os campos para prosseguir.") }
-
-});
-
-function cadastroSucesso(jsonRecebido) {
-
-console.log("Json recebido ao cadastrar");
-console.log(jsonRecebido);
-alert("Usuário cadastrado com sucesso")
-}
-
-function cadastroErro(statusRecebido){
-console.log("Erro ao cadastrar");
-console.log(statusRecebido);
-}
-
+const user = {
+    firstName:  document.getElementById("nome").value,
+    lastName:document.getElementById("apelido").value,
+    email: document.getElementById("email").value,
+    password:document.getElementById("senha").value
+    };
+    
+    let nomeCriarConta = document.getElementById("nome");
+    let apelidoCriarConta = document.getElementById("apelido");
+    let emailCriarConta = document.getElementById("email");
+    let senhaCriarConta = document.getElementById("senha");
+    //let repetirSenhaCriarConta = getElementById("repetirSenha");
+    
+    let botaoCadastro = document.getElementById("botaoCadastro")
+    
+    
+    botaoCadastro.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    
+    
+    const validator = (user) => {
+        Object.keys(user).every(key => !isEmpty(user[key]))
+        return Object.keys(user)
+    }
+    
+    const isEmpty = (target) =>{
+        return target == "" || target == " "
+       
+    }
+    
+    if(validator (user !=""))  {
+    user.firstName = nomeCriarConta.value;
+    user.lastName = apelidoCriarConta.value;
+    user.email = emailCriarConta.value;
+    user.password = senhaCriarConta.value;
+    
+    const requisicao = {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+    'content-type':'application/json'
+    },
+    };
+    
+    
+    fetch("https://ctd-todo-api.herokuapp.com/v1/users", requisicao)
+    . then((response) => {
+    if(response.status == 201) {
+    return response.json()
+    }
+    throw response;
+    
+    }).then(function (resposta) {
+    cadastroSucesso(resposta.jwt)
+    })
+    .catch(error => {
+    cadastroErro(error)
+    });
+    } 
+    else {
+    alert("Preencha todos os campos para prosseguir.") }
+    
+    });
+    
+    function cadastroSucesso(jsonRecebido) {
+    
+    console.log("Json recebido ao cadastrar");
+    console.log(jsonRecebido);
+    alert("Usuário cadastrado com sucesso")
+    }
+    
+    function cadastroErro(statusRecebido){
+    console.log("Erro ao cadastrar");
+    console.log(statusRecebido);
+    }
+    
+    
